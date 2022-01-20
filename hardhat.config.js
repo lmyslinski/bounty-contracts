6eea2f5f2e816b98836c17b267b89043f0a5c900
lmyslinski <lukasz.myslinski@datarobot.com>
2022-01-20 12:58:36 +0100

    Added ropsten config and license

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const supervisorPkey = process.env.SUPERVISOR_PKEY;
+if (!supervisorPkey) {
+  throw new Error("Please set your MNEMONIC in a .env file");
+}
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+const infuraProjectKey = "373543e00dc9456b98aec7048949799c";
+const infuraApiKey = process.env.INFURA_API_KEY;
+
+if (!infuraApiKey) {
+  throw new Error("Please set your INFURA_API_KEY in a .env file");
+}
+
 module.exports = {
   solidity: "0.8.4",
   paths: {
@@ -14,6 +20,10 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${infuraProjectKey}`,
+      accounts: [supervisorPkey]
     }
   }
 };
